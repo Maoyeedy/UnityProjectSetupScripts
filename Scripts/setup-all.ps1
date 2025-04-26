c0863194b7188f18b6c606d601d46f848281aae2
Maoyeedy <maoyeedy@gmail.com>
2025-04-26 04:10:11 +0300

    Fix Setup-All

--- Scripts/setup-all.ps1
+++ Scripts/setup-all.ps1
@@ -1,24 +1,22 @@
-Write-Host $PSScriptRoot
-
-# if (Test-Path -Path ".\Setup.bat") {
-#     # Ensure following scripts execute at project root
-#     Set-Location (Split-Path -Parent $PSScriptRoot)
-# }
+if (Test-Path -Path ".\Setup.bat") {
+    # Ensure runs at Unity project root
+    Set-Location "$PSScriptRoot\..\.."
+}
 
 
-# . "$PSScriptRoot\functions.ps1"
+. "$PSScriptRoot\functions.ps1"
 
-# Write-Host "`nStarting Project setup for:" -ForegroundColor Cyan
-# Write-Host $PWD
+Write-Host "`nStarting Project setup for:" -ForegroundColor Cyan
+Write-Host $PWD
 
-# & "$PSScriptRoot\setup-git-lfs.ps1"
+& "$PSScriptRoot\setup-git-lfs.ps1"
 
-# & "$PSScriptRoot\setup-git-options.ps1"
+& "$PSScriptRoot\setup-git-options.ps1"
 
-# & "$PSScriptRoot\setup-submodules.ps1"
+& "$PSScriptRoot\setup-submodules.ps1"
 
-# & "$PSScriptRoot\setup-unityyamlmerge.ps1"
+& "$PSScriptRoot\setup-unityyamlmerge.ps1"
 
-# & "$PSScriptRoot\setup-merge-rules.ps1"
+& "$PSScriptRoot\setup-merge-rules.ps1"
 
-# Write-Host "`nProject setup completed!" -ForegroundColor Cyan
+Write-Host "`nProject setup completed!" -ForegroundColor Cyan
