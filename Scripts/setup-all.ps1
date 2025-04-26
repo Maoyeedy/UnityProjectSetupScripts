a4629d43e65ceff895aac64c97cc3634b7d711f7
Maoyeedy <maoyeedy@gmail.com>
2025-04-26 04:07:21 +0300

    Disable setup-all

--- Scripts/setup-all.ps1
+++ Scripts/setup-all.ps1
@@ -1,21 +1,24 @@
-if (Test-Path -Path ".\Setup.bat") {
-    # Ensure following scripts execute at project root
-    Set-Location (Split-Path -Parent $PSScriptRoot)
-}
+Write-Host $PSScriptRoot
 
-. "$PSScriptRoot\functions.ps1"
+# if (Test-Path -Path ".\Setup.bat") {
+#     # Ensure following scripts execute at project root
+#     Set-Location (Split-Path -Parent $PSScriptRoot)
+# }
 
-Write-Host "`nStarting Project setup for:" -ForegroundColor Cyan
-Write-Host $PWD
 
-& "$PSScriptRoot\setup-git-lfs.ps1"
+# . "$PSScriptRoot\functions.ps1"
 
-& "$PSScriptRoot\setup-git-options.ps1"
+# Write-Host "`nStarting Project setup for:" -ForegroundColor Cyan
+# Write-Host $PWD
 
-& "$PSScriptRoot\setup-submodules.ps1"
+# & "$PSScriptRoot\setup-git-lfs.ps1"
 
-& "$PSScriptRoot\setup-unityyamlmerge.ps1"
+# & "$PSScriptRoot\setup-git-options.ps1"
 
-& "$PSScriptRoot\setup-merge-rules.ps1"
+# & "$PSScriptRoot\setup-submodules.ps1"
 
-Write-Host "`nProject setup completed!" -ForegroundColor Cyan
+# & "$PSScriptRoot\setup-unityyamlmerge.ps1"
+
+# & "$PSScriptRoot\setup-merge-rules.ps1"
+
+# Write-Host "`nProject setup completed!" -ForegroundColor Cyan
