0f1af69cd6d3324068fa0ab341bd8e925b42706e
Maoyeedy <maoyeedy@gmail.com>
2025-05-01 20:46:06 +0300

    Update submodule update

--- Scripts/setup-submodules.ps1
+++ Scripts/setup-submodules.ps1
@@ -11,7 +11,7 @@ try{
     $postMergeHookPath = ".git/hooks/post-merge"
     $lineToAdd = @"
 echo -e '\033[1;33mUpdating Submodules...\033[0m'
-git submodule update --init --recursive --remote
+git submodule update --init --recursive
 echo -e '\033[1;32mUpdate Complete\033[0m'
 "@
 
