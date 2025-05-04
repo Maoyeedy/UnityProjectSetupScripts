90e258415d164d1e277bb08242be480eee318514
Maoyeedy <maoyeedy@gmail.com>
2025-05-04 16:23:41 +0300

    Allow execute pwsh script

--- Setup.bat
+++ Setup.bat
@@ -1,2 +1,3 @@
 @echo off
+powershell -Command "Set-ExecutionPolicy Unrestricted -Scope CurrentUser -Force"
 powershell -Command "Start-Process powershell -Verb RunAs -Args '-noe -nop -c \"cd ''%~dp0''; .\Scripts\setup-all.ps1\"'"
