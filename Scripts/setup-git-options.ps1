f6e49c9fb1e2ce4427cdccfc11185063a343efa9
Yidi Mao <maoyeedy@gmail.com>
2025-05-06 00:01:19 +0300

    Update setup-git-options.ps1

--- Scripts/setup-git-options.ps1
+++ Scripts/setup-git-options.ps1
@@ -4,10 +4,10 @@ if (-not (Get-Command -Name 'Get-UnityVersion' -ErrorAction SilentlyContinue)) {
 
 Write-Host "`nConfiguring Git Options..." -ForegroundColor Yellow
 try {
-    git config core.autocrlf true
-    Write-Host "core.autocrlf set to true" -ForegroundColor DarkGray
-    git config core.safecrlf false
-    Write-Host "core.safecrlf set to false" -ForegroundColor DarkGray
+    git config core.autocrlf input
+    Write-Host "core.autocrlf set to input" -ForegroundColor DarkGray
+    git config core.safecrlf true
+    Write-Host "core.safecrlf set to true" -ForegroundColor DarkGray
     git config core.longpaths true
     Write-Host "core.longpaths set to true" -ForegroundColor DarkGray
 
